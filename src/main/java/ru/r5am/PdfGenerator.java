292580f5ba425a80fb9eeb5378051e44a3b59349
Aleksandr Jashhuk <richmanfx@yandex.ru>
2017-08-06 18:09:25 +0300

    Русский TTF-шрифт в PDF-документе, в буфер обмена в Linux копируется.

--- src/main/java/ru/r5am/PdfGenerator.java
+++ src/main/java/ru/r5am/PdfGenerator.java
@@ -1,6 +1,5 @@
 package ru.r5am;
 
-
 import com.itextpdf.io.font.FontConstants;
 import com.itextpdf.kernel.font.PdfFont;
 import com.itextpdf.kernel.font.PdfFontFactory;
@@ -13,13 +12,10 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 
-
 public class PdfGenerator {
 
     private static final String PDF_FILE = "HelloDmitri.pdf";
     private static int FONT_SIZE_SMALL = 16;
-    private static int FONT_SIZE_BIG = 32;
-    private static int OFFSET = 40;
 
     public static void main(String[] args) throws IOException {
 
@@ -35,11 +31,12 @@ public class PdfGenerator {
 
         Document document = new Document(pdf);
 
-        PdfFont font = PdfFontFactory.createFont(FontConstants.TIMES_ROMAN, "UTF-8", true);
-        PdfFont bold = PdfFontFactory.createFont(FontConstants.HELVETICA_BOLD);
-//        Font font = FontFactory.getFont(FONT, "Cp1251", BaseFont.EMBEDDED);
 
-        document.add(new Paragraph("Privet! Привет, Дима!").setFont(font));
+        PdfFont font = PdfFontFactory.createFont(FontConstants.TIMES_ROMAN);
+        PdfFont russianFont = PdfFontFactory.createFont("src/main/resources/FreeSans/FreeSans.ttf", "CP1251", true);
+
+        document.setFont(font);
+        document.add(new Paragraph("Privet! Привет, Дима!").setFontSize(FONT_SIZE_SMALL).setFont(russianFont));
 
         document.close();
     }
